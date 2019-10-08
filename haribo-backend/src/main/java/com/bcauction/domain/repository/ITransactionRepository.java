97c1d32910e1a7e173dc18cb31697b77caaf6e04
임현철 <lhc6005@hanmail.net>
2019-10-08 17:54:40 +0900

    191008 txlist DB cunnect

--- haribo-backend/src/main/java/com/bcauction/domain/repository/ITransactionRepository.java
+++ haribo-backend/src/main/java/com/bcauction/domain/repository/ITransactionRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface ITransactionRepository {
     List<Transaction> 목록조회();
+    List<Transaction> 목록조회10();
     Transaction 조회(String hash);
     List<Transaction> 조회By주소(String 주소);
     long 추가(Transaction 트랜잭션);
